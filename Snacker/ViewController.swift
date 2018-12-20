dd875be5f5ff359c155925c0915a9934e5f8709f
Etash Guha <etashguha@Etashs-MacBook-Pro.local>
2018-12-19 22:56:44 -0500

    location taken into effect

--- Snacker/ViewController.swift
+++ Snacker/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 import Firebase
 import os.log
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController,  UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+class ViewController: UIViewController,  UINavigationControllerDelegate, UIImagePickerControllerDelegate, CLLocationManagerDelegate{
+    
 
     @IBOutlet weak var foodDescription: UITextField!
     @IBOutlet weak var save: UIBarButtonItem!
     @IBOutlet weak var foodImage: UIImageView!
+    var locationManager = CLLocationManager()
+    var location: CLLocation!
+    var ref: DatabaseReference!
     
     @IBAction func importImage(_ sender: Any) {
         let image = UIImagePickerController()
@@ -33,18 +39,26 @@ class ViewController: UIViewController,  UINavigationControllerDelegate, UIImage
         self.dismiss(animated: true, completion: nil)
     }
     
-    var ref: DatabaseReference!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         ref = Database.database().reference()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations[0]
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let imageData = foodImage.image?.jpegData(compressionQuality: 0.01)
         let strBase64 = imageData?.base64EncodedString(options: .lineLength64Characters)
-        let stuffToPost = ["addasdfasdfress", foodDescription.text!, strBase64] as [Any]
+        let stuffToPost = ["addasdfasdfress", foodDescription.text!, strBase64!, location.coordinate.latitude, location.coordinate.longitude] as [Any]
         ref.child("Posts").childByAutoId().setValue(stuffToPost)
     }
 }
